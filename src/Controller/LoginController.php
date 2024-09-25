28b490f9383d1a3fc7999b93fb698c1162bfe0f3
Jacpio <jacpio512383881@gmail.com>
2024-09-25 13:00:07 +0200

    Add little fix

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Cake\Http\Exception\BadRequestException;
 use Cake\Http\Response;
 use Cake\ORM\TableRegistry;
 use Cake\View\JsonView;
@@ -32,10 +33,17 @@ class LoginController extends AppController
     {
         return [JsonView::class];
     }
+
     public function getToken(): Response
     {
+
         $this->request->allowMethod(['post']);
-        $user = $this->request->getData();
+        try {
+            $user = $this->request->getData();
+        } catch (BadRequestException $e) {
+            return $this->response->withStatus(400)
+                ->withStringBody(json_encode(['message' => 'Invalid query']));
+        }
         $userByUsername = $this->Users->find('all',
             [
                 'fields' => ['id', 'username', 'password'],
@@ -44,34 +52,40 @@ class LoginController extends AppController
         )->first();
         if (!$userByUsername) {
             return $this->response->withStatus(401)
-                ->withStringBody(json_encode(['message'=>'Invalid username or password']));
+                ->withStringBody(json_encode(['message' => 'Invalid username or password']));
         }
-        if (password_verify($user['password'],  $userByUsername['password'])){
-            $token = uniqid('',true);
+        if (password_verify($user['password'], $userByUsername['password'])) {
+            $token = uniqid('', true);
             $userByID = $this->Users->get($userByUsername['id']);
             $userWithToken = $this->Users->patchEntity($userByID, ['token' => $token]);
 
             if (!$this->Users->save($userWithToken)) {
-               return $this->response->withStatus(500)
-                   ->withStringBody(json_encode(['message'=>'Something went wrong']));
+                return $this->response->withStatus(500)
+                    ->withStringBody(json_encode(['message' => 'Something went wrong']));
             }
             return $this->response->withStatus(200)
-                ->withHeader('X-Expires-After', '3600')
+                ->withHeader('X-Expires-After',  '3600')
                 ->withDisabledCache()
-                ->withStringBody(json_encode($userWithToken));
-        }else{
+                ->withStringBody(json_encode(['token' => $token, 'userdata' => $userWithToken]));
+        } else {
             return $this->response->withStatus(401)
-                ->withStringBody(json_encode(['message'=>'Invalid username or password']));
+                ->withStringBody(json_encode(['message' => 'Invalid username or password']));
         }
+
     }
+
     public function deleteToken()
     {
 
     }
-    public function editName($id = null){
+
+    public function editName($id = null)
+    {
 
     }
-    public function getInformation($id = null){
+
+    public function getInformation($id = null)
+    {
 
     }
 }
