8f5687f2339afbf6fa412694ec6a20272a1dfcea
Kłysiu Kłysiński <62438030+Kubsj@users.noreply.github.com>
2024-09-29 03:41:34 +0200

    Update hackyeah.sql

--- sql/hackyeah.sql
+++ sql/hackyeah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Wrz 28, 2024 at 01:14 PM
+-- Generation Time: Wrz 29, 2024 at 12:18 AM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.2.12
 
@@ -20,6 +20,97 @@ SET time_zone = "+00:00";
 --
 -- Database: `hackyeah`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `bonds`
+--
+
+CREATE TABLE `bonds` (
+  `id` int(11) NOT NULL,
+  `series` varchar(255) NOT NULL,
+  `price` double NOT NULL,
+  `interest` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bonds`
+--
+
+INSERT INTO `bonds` (`id`, `series`, `price`, `interest`) VALUES
+(1, 'E', 100, 6.5),
+(2, 'D', 100, 6),
+(3, 'C', 100, 6),
+(4, 'B', 100, 6),
+(5, 'A', 100, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `currency`
+--
+
+CREATE TABLE `currency` (
+  `id` int(11) NOT NULL,
+  `name` varchar(11) NOT NULL,
+  `ask` double NOT NULL,
+  `bid` double NOT NULL,
+  `request_date` datetime NOT NULL,
+  `effective_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `currency`
+--
+
+INSERT INTO `currency` (`id`, `name`, `ask`, `bid`, `request_date`, `effective_date`) VALUES
+(1, 'usd', 3.8713, 3.7947, '2024-09-28 13:41:09', '2024-09-27'),
+(2, 'eur', 4.3129, 4.2275, '2024-09-28 13:59:47', '2024-09-27');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `ores`
+--
+
+CREATE TABLE `ores` (
+  `id` int(11) NOT NULL,
+  `name` varchar(11) NOT NULL,
+  `buy` double NOT NULL,
+  `sell` double NOT NULL,
+  `request_date` datetime NOT NULL,
+  `measurment_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `ores`
+--
+
+INSERT INTO `ores` (`id`, `name`, `buy`, `sell`, `request_date`, `measurment_date`) VALUES
+(1, 'gold coin', 10427, 9868, '2024-09-28 18:06:08', '2024-09-28'),
+(2, 'silver coin', 114.6376, 97.79, '2024-09-28 18:07:46', '2024-09-28');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `stocks`
+--
+
+CREATE TABLE `stocks` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` double NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `stocks`
+--
+
+INSERT INTO `stocks` (`id`, `name`, `price`, `date`) VALUES
+(1, 'tesla', 998.5, '2024-09-28 23:24:58'),
+(2, 'microsoft', 1669.2, '2024-09-28 23:25:28');
 
 -- --------------------------------------------------------
 
@@ -57,6 +148,30 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `points`, `two_facto
 --
 
 --
+-- Indeksy dla tabeli `bonds`
+--
+ALTER TABLE `bonds`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `currency`
+--
+ALTER TABLE `currency`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `ores`
+--
+ALTER TABLE `ores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `stocks`
+--
+ALTER TABLE `stocks`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeksy dla tabeli `users`
 --
 ALTER TABLE `users`
@@ -65,6 +180,30 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `bonds`
+--
+ALTER TABLE `bonds`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `currency`
+--
+ALTER TABLE `currency`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `ores`
+--
+ALTER TABLE `ores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `stocks`
+--
+ALTER TABLE `stocks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
